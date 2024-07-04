39b3cf5fbb0ecee4e88a443dcb4e27c0580f2d12
Sonia Esquer <sonia.esquer-banos@capgemini.com>
2024-07-04 12:19:16 +0200

    Conexion a Mongo con usuario invitado

--- env/dev.js
+++ env/dev.js
@@ -1,7 +1,6 @@
 'use strict'
 
 module.exports = {
-	db: 'mongodb://localhost:27017/ListaCompra',
-  uriDb: "mongodb+srv://nemy:serloquesoy.1978@apps.p4rtujn.mongodb.net/ListaCompra?retryWrites=true&w=majority",
+  uriDb: "mongodb+srv://invitado:VfJvlvTnd0GlEWpK@apps.p4rtujn.mongodb.net/ListaCompra?retryWrites=true&w=majority",
   port: 3030
 }
